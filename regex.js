6b2d352c3d7ffd3517fb04dfe54309aac37f12b4
attah ifeanyi <attahsixtus6@gmail.com>
2023-01-15 18:12:38 +0100

    first commit

---
+++ regex.js
@@ -0,0 +1,18 @@
+function VisaCardNumber(inputtxt) {
+  //This is a visa card that Allows for either 13 or 16 digits between 0 and 9 and also must start with 4
+  const cardno = /^4[0-9]{12}(?:[0-9]{3})?$/;
+  //if (Number.isNaN(parseInt(inputtxt))) {
+  // return true;
+  return cardno.test(inputtxt);
+}
+
+function validateVisaCardNumber(cardnumber) {
+  let cardType = "invalid visa card, please enter a valid card number";
+  if (VisaCardNumber(cardnumber)) {
+    cardType = " valid visa card";
+  }
+  return cardType;
+}
+
+const valid = validateVisaCardNumber(400234456567894);
+console.log(valid);
